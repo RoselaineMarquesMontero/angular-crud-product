b363da1adda28eb25c7024e94893c4835f109c71
Roselaine Marques <roselaine.marques@liferay.com>
2021-01-25 18:20:27 +0100

    first commit

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,37 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../product.service';
+import { Product } from '../product.model';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: 'Product Test',
+    price: 125.98
+  }
+
+  constructor(private productService:ProductService, private router: Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  createProduct(): void{
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Sucess');
+      this.router.navigate(['/products']);
+    });
+   ;;
+  }
+
+  cancel(): void{
+    this.router.navigate(['/products']);
+  }
+
+}
